7f1afcd030db6cb06afed2d91c5285d73602274c
Louisa Casanave <Louisa.casanave@gmail.com>
2015-01-29 12:59:38 -0500

    Update censorship.rb

--- censorship.rb
+++ censorship.rb
@@ -1,12 +1,4 @@
 puts "Hello overworried mothers and busybodies. I am censor-bot, and this is my here ship, the good ship Censorship. Just put in the original smut that you happened to see your kids reading and I will make all the adjustments required." 
 
-doc=gets.chomp
-
-target_words.each do |a_word|
-     if    a_word=="penis" puts "peepee"
-     or    a_word=="vagina" puts "ladyparts"
-     elsif prints a_word
-     end 
-end 
-
+#just an idea for fun to replace curse words with their milder counterparts
 
