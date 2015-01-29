b36cf22788d7f5f5a6302ee9f123fe2b80251e8d
Louisa Casanave <Louisa.casanave@gmail.com>
2015-01-29 13:00:31 -0500

    Update prime_num_finder.rb

--- prime_num_finder.rb
+++ prime_num_finder.rb
@@ -1,16 +1,1 @@
-puts "punch in a number between 1 and 100 and I will try to determine if it is a prime number" 
-n=gets
-f=0+1
-
-def cheeter_check
-   if n<100 and n>1 puts "let me think about that"
-   else puts "you cheated! that's not between 1 and 100"
-   end 
-end
-
-def factor_check
-if f=!1 or f=!n
-and n % f ==0
-puts "that is not a prime number. sorry" 
-elsif f
-
+#just a reminder to myself to make this
